fc19871b08335f631804dc297e86b98e1b39b4e3
nianlu <nianlushu@gmail.com>
2021-11-09 21:46:09 -0500

    print using spectrogram

--- src/Recorder.js
+++ src/Recorder.js
@@ -8,6 +8,71 @@ import * as tf from '@tensorflow/tfjs'
 
 import * as math from 'mathjs'
 
+import Spectrogram from 'spectrogram'
+
+// const customSpectrogram = 
+
+Spectrogram.prototype._draw = function(array, canvasContext) {
+    if (this._paused) {
+      return false;
+    }
+
+    var canvas = canvasContext.canvas;
+    var width = canvas.width;
+    var height = canvas.height;
+    var tempCanvasContext = canvasContext._tempContext;
+    var tempCanvas = tempCanvasContext.canvas;
+    tempCanvasContext.drawImage(canvas, 0, 0, width, height);
+
+    for (var i = 0; i < array.length; i++) {
+      var value = array[i];
+      canvasContext.fillStyle = this._getColor(value);
+      if (this._audioEnded) {
+        canvasContext.fillStyle = this._getColor(0);
+      }
+      canvasContext.fillRect(width - 1, height - i, 1, 1);
+      // canvasContext.fillRect(width - 4, height - i, 4, 1);
+    }
+
+    canvasContext.translate(-1, 0);
+    // canvasContext.translate(-4, 0);
+    // draw prev canvas before translation
+    canvasContext.drawImage(tempCanvas, 0, 0, width, height, 0, 0, width, height);
+    canvasContext.drawImage(tempCanvas, 0, 0, width, height, 0, 0, width, height);
+    // reset transformation matrix
+    // canvasContext.setTransform(1, 0, 0, 1, 0, 0);
+    canvasContext.setTransform(1, 0, 0, 1, 0, 0);
+
+    this._baseCanvasContext.drawImage(canvas, 0, 0, width, height);
+};
+
+Spectrogram.prototype.resume = function(offset) {
+  var source = this._sources[Object.keys(this._sources)[0]];
+  console.log('resume', source, this._sources)
+  this._paused = false;
+  if (this._pausedAt) {
+    console.log('resume2', this._pausedAt, source)
+    this.connectSource(source.audioBuffer, source.audioContext);
+    this.start(offset || (this._pausedAt / 1000));
+  }
+};
+
+Spectrogram.prototype._startMediaStreamDraw = function(analyser, canvasContext) {
+  window.requestAnimationFrame(this._startMediaStreamDraw.bind(this, analyser, canvasContext));
+  var audioData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(audioData);
+  this._draw(audioData, canvasContext);
+  var audioData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(audioData);
+  this._draw(audioData, canvasContext);
+  var audioData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(audioData);
+  this._draw(audioData, canvasContext);
+  var audioData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(audioData);
+  this._draw(audioData, canvasContext);
+};
+
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
 // https://stackoverflow.com/questions/50431236/use-getusermedia-media-devices-in-reactjs-to-record-audio/50440682
 // https://air.ghost.io/recording-to-an-audio-file-using-html5-and-js/
@@ -25,152 +90,59 @@ const Recorder = props => {
   // const [ueRun, setUeRun] = useState(false)
   const [ueData, setUeData] = useState()
 
+  const [spectrogram, setSpectrogram] = useState()
+
   useEffect(() => {
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const spectro = Spectrogram(document.getElementById('canvas'), {
+      canvas: {
+        width: function() {
+          return window.innerWidth;
+        },
+        height: 500
+      },
+      audio: {
+        enable: true
+      },
+    });
+
+    // try {
+    //   audioContext = new AudioContext();
+    // } catch (e) {
+    //   alert('No web audio support in this browser!');
+    // }
+
     navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => {
-      console.log('stream')
+      // console.log('stream')
 
       const source = audioCtx.createMediaStreamSource(stream);
       const analyser = audioCtx.createAnalyser()
+
+      analyser.smoothingTimeConstant = 0
+      analyser.fftSize = 2048 * 2
+
       source.connect(analyser);
-      analyser.connect(audioCtx.destination)
+      // analyser.connect(audioCtx.destination)
+      // setUeAnalyser(analyser)
+      // console.log('setanalyser')
 
-      setUeAnalyser(analyser)
-      console.log('setanalyser')
+      spectro.connectSource(analyser, audioCtx);
+      // spectro.start();
+      setSpectrogram(spectro)
     })
+
   }, [])
-
-  const onUeStart = () => {
-
-    var bufferLength = ueAnalyser.frequencyBinCount;
-    console.log('ona', bufferLength)
-    // var dataArray = new Uint8Array(bufferLength);
-    let data = []
-
-    // every 0.05s
-    const iid = setInterval(() => {
-      var dataArray = new Uint8Array(bufferLength);
-      ueAnalyser.getByteFrequencyData(dataArray)
-      // console.log('setinterval', dataArray)
-      // const c = data.push(dataArray)
-      // if (c > 100)
-      //   data.shift()
-      const max = Math.max.apply(null, dataArray)
-      console.log('recording max', max)
-      if (data.length > 1 || max > 70)
-        if (data.push(dataArray) > 500)
-          data.shift()
-      // setUeData(ueData.concat([dataArray]))
-    }, 10);
-
-    setUeData(data)
-    setUeIId(iid)
-    console.log('iid', iid)
-    setStatus('start')
-    setInfo('recording')
-  }
-
-  const onUeStop = () => {
-    clearInterval(ueIId)
-    console.log('ddd', ueData)
-    setStatus('stop')
-    setInfo('finished')
-
-    console.log('ddd', JSON.stringify(ueData.map(d => Array.from(d))))
-    // const audioURL = window.URL.createObjectURL(blob);
-    // const audioURL = window.URL.createObjectURL(ueData);
-    const audioURL = window.URL.createObjectURL(new Blob([JSON.stringify(ueData.map(d => Array.from(d)))]), {type : 'application/json'});
-    // append videoURL to list of saved videos for rendering
-    // const audios = this.state.audios.concat([audioURL]);
-    console.log(audioURL)
-
-    var canvas = document.querySelector('.visualizer');
-    var canvasCtx = canvas.getContext("2d");
-    // canvas.setAttribute('width',intendedWidth);
-    // const WIDTH = canvas.width;
-    // const HEIGHT = canvas.height;
-    // const WIDTH = 640;
-    const wp = 2
-    const WIDTH = wp * ueData.length;
-    // const HEIGHT = 1024;
-    const wh = 2
-    const HEIGHT = 400;
-
-    // // create imageData object
-    // var idata = canvasCtx.createImageData(WIDTH, HEIGHT);
-    // // set our buffer as source
-    // idata.data.set(ueData);
-    // // update canvas with new data
-    // canvasCtx.putImageData(idata, 0, 0);
-
-    console.log('ueData', ueData.length)
-    // canvasCtx.clearRect(0, 0, 800, HEIGHT);
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    const flatD = ueData.flatMap(d => Array.from(d))
-    console.log('flat', flatD)
-    const resultOfQuantile = math.quantileSeq(flatD, 0.95)
-    console.log('resultOfQuantile', resultOfQuantile)
-
-    console.log('start print')
-    ueData.forEach((d, i) => {
-      // canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-      // canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-      // const bufferLength = 1024
-      // var dy = (HEIGHT / bufferLength)
-      var dx = i * wp
-      d.forEach((c, j) => {
-        if (j > (HEIGHT/wh)) return
-        // var dy = HEIGHT - j
-        var dy = HEIGHT - j*wh
-        canvasCtx.fillStyle = 'rgb(' + (c > resultOfQuantile? (c-resultOfQuantile)*3+10 : 10) + ',10,10)'; // 255 //rgb(100, 10, 10)
-        canvasCtx.fillRect(dx, dy, wp, wh);
-      })
-
-      // var barWidth = (WIDTH / bufferLength) * 2.5;
-      // var barHeight;
-      // var x = 0;
-      // for(var i = 0; i < bufferLength; i++) {
-      //   barHeight = (d[i] )*2;
-      //   canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight+100) + ',50,50)';
-      //   canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-      //   x += barWidth + 1;
-      // }
-    })
-    console.log('end print')
-// https://github.com/calebj0seph/spectro
-  }
-
-  const onPrepare = async _ => {
-    // console.log('preparing', blob)
-    // const bbb = await blob.arrayBuffer()
-    // console.log('preparingbbb', bbb)
-    // const aaa = new Float32Array(bbb)
-    // console.log('aaa', aaa)
-    // const ttt = tf.tensor(aaa)
-    // console.log('tttt', ttt)
-
-    console.log('ppp', ueData)
-    const ttt = tf.tensor(ueData)
-    console.log('tttt', ttt)
-
-    pred(ttt)
-
-  }
   
-  const onSave = () => {
-    recorder && recorder.state === 'inactive' && 
-    console.log(recorder.state)
-    // console.log(chunk)
-    // console.log(blob)
+  const onStart = () => {
+    spectrogram && status === 'stop'? spectrogram.start() : spectrogram.resume(10)
+    // spectrogram && spectrogram.clear()
+    // spectrogram && spectrogram.start()
+    setStatus('start')
+  }
 
-    // const audioURL = window.URL.createObjectURL(blob);
-    // append videoURL to list of saved videos for rendering
-    // const audios = this.state.audios.concat([audioURL]);
-
-    // console.log(audioURL)
+  const onStop = () => {
+    spectrogram && spectrogram.pause()
+    setStatus('pause')
   }
 
   const mobileButtonStyle = {
@@ -193,14 +165,14 @@ const Recorder = props => {
     <div>
       <Word />
       <div style={{marginBottom: '1rem'}}>{info}</div>
-      {/* <button className='ctc-button' onClick={onUeStart}>Start</button>
-      <button className='ctc-button' onClick={onUeStop}>Stop</button> */}
+      {/* <button className='ctc-button' onClick={onStart}>Start</button>
+      <button className='ctc-button' onClick={onStop}>Stop</button> */}
       {/* <button className='ctc-button' onClick={onCancel}>cancel</button>
       <button className='ctc-button' onClick={onRestart}>restart</button> */}
       {isMobile?
         <button className='ctc-mobile-record'
-          onTouchStart={status === 'stop'? onUeStart : _ => {}}
-          onTouchEnd={status === 'start'? onUeStop : _ => {}}
+          onTouchStart={status === 'stop'? onStart : _ => {}}
+          onTouchEnd={status === 'start'? onStop : _ => {}}
         >
           {status === 'stop'?
             'start'
@@ -209,17 +181,20 @@ const Recorder = props => {
           }
         </button>
       : status === 'stop'?
-        <button className='ctc-button' onClick={onUeStart}>start</button>
+        <button className='ctc-button' onClick={onStart}>start</button>
+      : status === 'pause'?
+        <button className='ctc-button' onClick={onStart}>start</button>
       : status === 'start'?
-        <button className='ctc-button' onClick={onUeStop}>stop</button>
+        <button className='ctc-button' onClick={onStop}>stop</button>
       :
-        <button className='ctc-button' disabled={true} onClick={onUeStop}>stop</button>
+        <button className='ctc-button' disabled={true} onClick={onStop}>stop</button>
       }
       <br />
       {status === 'stop' && ueData &&
         <span>{ueData.length}</span>
       }
       <br />
+      <canvas id='canvas'></canvas> 
       <canvas className="visualizer" width="640" height="640"></canvas> 
       {/* <button disabled={!link} onClick={onPrepare}>prepare</button> */}
       {/* <div>
@@ -233,7 +208,7 @@ const Recorder = props => {
 
         )}
       </div> */}
-      <button onClick={onPrepare}>prepare</button>
+      {/* <button onClick={onPrepare}>prepare</button> */}
       <button onClick={_ => pred(ueData)}>test predict</button>
       {/* <button disabled={!link}><a href={link}>download</a></button> */}
       {/* <button disabled={!link} onClick={upload}>upload</button> */}
